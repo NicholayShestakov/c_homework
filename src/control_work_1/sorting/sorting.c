460797382043d21f3edb429bf7df91f8d4266d09
Nicholay Shestakov <nicholas.shestakov@gmail.com>
2025-10-13 14:14:20 +0300

    Add commentary for gnome sorting function

--- src/control_work_1/sorting/sorting.c
+++ src/control_work_1/sorting/sorting.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Алгоритм гномьей сортировки. Принимает указатель на массив и длину массива.
 void gnomeSort(int* array, int size)
 {
     int current = 1;
