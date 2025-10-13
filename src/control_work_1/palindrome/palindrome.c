d92f767b551ce53dfe151bb2c71e097d0a763705
Nicholay Shestakov <nicholas.shestakov@gmail.com>
2025-10-13 13:51:56 +0300

    Add string palindrome definder

---
+++ src/control_work_1/palindrome/palindrome.c
@@ -0,0 +1,34 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    const char str[] = "Lorem m e r o L";
+    int startPointer = 0;
+    int endPointer = strlen(str) - 1;
+    bool isPalindrome = true;
+
+    while (startPointer < endPointer) {
+        while (str[startPointer] == ' ') {
+            startPointer++;
+        }
+        while (str[endPointer] == ' ') {
+            endPointer--;
+        }
+
+        if (str[startPointer] != str[endPointer]) {
+            isPalindrome = false;
+            break;
+        }
+
+        startPointer++;
+        endPointer--;
+    }
+
+    if (isPalindrome) {
+        printf("String is palindrome. \n");
+    } else {
+        printf("String is not palindrome. \n");
+    }
+}
