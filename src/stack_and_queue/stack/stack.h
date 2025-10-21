ceeeb02df60f409dd711964c0f065f73ac0447b5
Nicholay Shestakov <nicholas.shestakov@gmail.com>
2025-10-21 19:58:39 +0300

    Update stack header file

--- src/stack_and_queue/stack/stack.h
+++ src/stack_and_queue/stack/stack.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdbool.h>
+
 typedef struct StackNode StackNode;
 
 typedef struct Stack Stack;
@@ -8,7 +10,7 @@ typedef struct Stack Stack;
 // Returns pointer on the created stack.
 Stack* newStack();
 
-// Deletes stack.
+// Deletes stack. Frees all memory used in it.
 // Takes pointer on the stack.
 void deleteStack(Stack* stack);
 
@@ -16,7 +18,7 @@ void deleteStack(Stack* stack);
 // Takes pointer on the stack and value for push.
 void push(Stack* stack, int value);
 
-// Deletes the top value of the stack and returns it.
+// Deletes the top value of the stack and returns it. Frees memory used for top node.
 // Takes pointer on the stack.
 // If the stack is empty returns -1.
 int pop(Stack* stack);
@@ -25,3 +27,7 @@ int pop(Stack* stack);
 // Takes pointer on the stack.
 // If the stack is empty returns -1.
 int peek(Stack* stack);
+
+// Returns true if the stack is empty and false if not.
+// Takes pointer on the stack.
+bool isEmpty(Stack* stack);
