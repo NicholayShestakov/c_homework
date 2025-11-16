c2fa36f6879c55cf04131f198149be70b4bf9647
Nicholay Shestakov <nicholas.shestakov@gmail.com>
2025-11-16 21:45:08 +0300

    Format * into right in sorted_list.h

--- src/sorted_list/sorted_list.h
+++ src/sorted_list/sorted_list.h
@@ -4,21 +4,21 @@ typedef struct List List;
 
 // Creates empty sorted list.
 // Returns pointer on it.
-List* createList();
+List *createList();
 
 // Deletes the sorted list and frees memory it used.
 // Takes pointer on the sorted list.
-void deleteList(List* list);
+void deleteList(List *list);
 
 // Appends value in the sorted list.
 // Takes pointer on the sorted list and value.
-void appendValue(List* list, int value);
+void appendValue(List *list, int value);
 
 // Deletes one element from the sorted list with taken value and frees memory it used.
 // Takes pointer on the sorted list and value.
 // If in the list have not elements with this value, prints it.
-void deleteValue(List* list, int value);
+void deleteValue(List *list, int value);
 
 // Prints the list.
 // Takes pointer on the sorted list.
-void printList(List* list);
+void printList(List *list);
