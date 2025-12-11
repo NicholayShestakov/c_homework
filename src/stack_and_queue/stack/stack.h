8e759a774e4c126238561e9f7cc8db37d5031498
Nicholay Shestakov <nicholas.shestakov@gmail.com>
2025-12-11 14:50:28 +0300

    Update stack header documentation

--- src/stack_and_queue/stack/stack.h
+++ src/stack_and_queue/stack/stack.h
@@ -17,15 +17,6 @@ void deleteStack(Stack* stack);
 void push(Stack* stack, int value);
 
 // Deletes the top value of the stack and returns it. Frees memory used for top node.
-<<<<<<< HEAD
-// Takes pointer on the stack.
-// If the stack is empty returns -1.
-int pop(Stack* stack);
-
-// Returns the top value of the stack.
-// Takes pointer on the stack.
-// If the stack is empty returns -1.
-=======
 // Takes pointer to the stack.
 // Don`t use if the stack is empty. It causes the error.
 // Check this with isEmpty before use this function.
@@ -35,7 +26,6 @@ int pop(Stack* stack);
 // Takes pointer to the stack.
 // Don`t use if the stack is empty. It causes the error.
 // Check this with isEmpty before use this function.
->>>>>>> cce4851 (Update stack header documentation)
 int peek(Stack* stack);
 
 // Returns true if the stack is empty and false if not.
