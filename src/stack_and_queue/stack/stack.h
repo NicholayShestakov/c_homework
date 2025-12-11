7424f8960e14c0c3519dbfe7e5685843954c5715
Nicholay Shestakov <nicholas.shestakov@gmail.com>
2025-12-11 11:15:51 +0300

    Update grammary in documentations

--- src/stack_and_queue/stack/stack.h
+++ src/stack_and_queue/stack/stack.h
@@ -5,27 +5,27 @@
 typedef struct Stack Stack;
 
 // Creates new stack.
-// Returns pointer on the created stack.
+// Returns pointer to the created stack.
 Stack* newStack();
 
 // Deletes stack. Frees all memory used in it.
-// Takes pointer on the stack.
+// Takes pointer to the stack.
 void deleteStack(Stack* stack);
 
 // Pushes new value on the top of the stack.
-// Takes pointer on the stack and value for push.
+// Takes pointer to the stack and value for push.
 void push(Stack* stack, int value);
 
 // Deletes the top value of the stack and returns it. Frees memory used for top node.
-// Takes pointer on the stack.
+// Takes pointer to the stack.
 // If the stack is empty returns -1.
 int pop(Stack* stack);
 
 // Returns the top value of the stack.
-// Takes pointer on the stack.
+// Takes pointer to the stack.
 // If the stack is empty returns -1.
 int peek(Stack* stack);
 
 // Returns true if the stack is empty and false if not.
-// Takes pointer on the stack.
+// Takes pointer to the stack.
 bool isEmpty(Stack* stack);
